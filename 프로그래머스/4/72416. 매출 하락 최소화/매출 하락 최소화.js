026ea16db84bca96281eee14d51c30d1accaf69f
IvoryShield <62371174+garlatonic@users.noreply.github.com>
2026-03-31 14:33:07 +0900

    [level 4] Title: 매출 하락 최소화, Time: 263.16 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.js
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.js
@@ -1,18 +1,16 @@
 function solution(sales, links) {
-  const teams = Array.from({ length: sales.length + 1 }, () => []); // 0인덱스는 원래 빈 값으로 두기
+  const teams = Array.from({ length: sales.length + 1 }, () => []); // 1-index
 
-  // 트리 생성
-  for (const link of links) {
-    const [leader, member] = link;
+  for (const [leader, member] of links) {
     teams[leader].push(member);
   }
 
   function dfs(number) {
     if (teams[number].length > 0) {
       let absentSum = 0;
-      let attendSum = sales[number - 1];
-      let minDiff = Infinity;
+      let attendSum = sales[number - 1]; // 0-index
       let hasAttendee = false;
+      let minDiff = Infinity;
 
       for (let i = 0; i < teams[number].length; i++) {
         const dp = dfs(teams[number][i]);
@@ -20,13 +18,10 @@ function solution(sales, links) {
         attendSum += Math.min(dp[0], dp[1]);
         minDiff = Math.min(dp[1] - dp[0], minDiff);
 
-        if (dp[1] < dp[0]) {
-          hasAttendee = true;
-        }
+        if (dp[1] < dp[0]) hasAttendee = true;
       }
 
       if (!hasAttendee) absentSum += minDiff;
-
       return [absentSum, attendSum];
     } else {
       return [0, sales[number - 1]];
