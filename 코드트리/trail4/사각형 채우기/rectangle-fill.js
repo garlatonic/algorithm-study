18cdbc45bf22197e77a28b4aa2539e5dc4a16fff
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-05-16 21:43:13 +0900

    [Passed] 사각형 채우기 (71ms, 20MB)

---
+++ 코드트리/trail4/사각형 채우기/rectangle-fill.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+const dp = Array(n + 1).fill(0);
+dp[1] = 1;
+dp[2] = 2;
+for (let i = 3; i < n + 1; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n]);
