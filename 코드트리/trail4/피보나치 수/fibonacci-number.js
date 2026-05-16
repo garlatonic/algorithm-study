07ec1b7990fa696269faa2e95b217c393ba5dff6
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-05-16 21:27:02 +0900

    [Passed] 피보나치 수 (67ms, 10MB)

---
+++ 코드트리/trail4/피보나치 수/fibonacci-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+const dp = Array(n + 1).fill(0);
+dp[1] = 1;
+dp[2] = 1;
+
+for (let i = 3; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+console.log(dp[n]);
