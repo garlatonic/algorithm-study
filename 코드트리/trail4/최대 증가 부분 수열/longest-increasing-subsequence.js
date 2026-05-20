bedb00831b5dcd9fca75908dfe398a6c54e463ab
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-05-20 14:35:45 +0900

    [Passed] 최대 증가 부분 수열 (72ms, 11MB)

---
+++ 코드트리/trail4/최대 증가 부분 수열/longest-increasing-subsequence.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const a = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+const sorted = a.map((v, i) => [v, i]);
+sorted.sort((a, b) => a[0] - b[0]);
+
+const dp = Array(n).fill(1);
+dp[0] = 1;
+
+for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (a[i] > a[j]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+}
+console.log(Math.max(...dp))
