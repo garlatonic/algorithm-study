c00d14d3055eb0ddf0e5e74be7d44a58ccc6b3f8
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-05-24 14:52:50 +0900

    [Passed] 1, 2, 5 더하기 (62ms, 10MB)

--- 코드트리/trail4/1, 2, 5 더하기/1-2-5-plus.js
+++ 코드트리/trail4/1, 2, 5 더하기/1-2-5-plus.js
@@ -3,17 +3,14 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 // Please write your code here.
 const MOD = 10007;
-const dp = Array(n + 1).fill(-Infinity);
-dp[0] = 1;
+const dp = Array(n + 1).fill(0);
+dp[0] = 1; // 아무 수열도 선택하지 않는 방법 1가지
 
 for (let i = 1; i <= n; i++) {
-    let count = 0;
     for (const num of [1, 2, 5]) {
-        if (i - num < 0) continue;
-        if (dp[i - num] === -Infinity) continue;
-        count = (count + dp[i - num]) % MOD;
+        if (i - num < 0) continue; // 고른 숫자가 합 i보다 클 때에는 선택하지 않음
+        dp[i] = (dp[i] + dp[i - num]) % MOD; // i - num를 만드는 방법에 num을 붙여 i를 만드는 경우
     }
-    dp[i] = count;
 }
 
 console.log(dp[n]);
