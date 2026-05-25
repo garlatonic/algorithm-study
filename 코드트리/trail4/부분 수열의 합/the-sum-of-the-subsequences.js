96ca26d9da5133c4d7d834e93d74dd9ffb7887b8
IVORY <62371174+garlatonic@users.noreply.github.com>
2026-05-25 21:25:35 +0900

    [Passed] 부분 수열의 합 (84ms, 14MB)

--- 코드트리/trail4/부분 수열의 합/the-sum-of-the-subsequences.js
+++ 코드트리/trail4/부분 수열의 합/the-sum-of-the-subsequences.js
@@ -5,15 +5,22 @@ const [n, m] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
 // Please Write your code here.
-const dp = Array(m + 1).fill(-Infinity);
-dp[0] = 0;
+const dp = Array.from(Array(n + 1), () => Array(m + 1).fill(false)); // i번째 원소까지 고려할 때 부분 수열의 합이 j가 될 수 있다
+dp[0][0] = true; // 아무것도 고르지 않은 상태, 0인 상태는 항상 가능
 
-for (let j = 0; j < n; j++) {
-    for (let i = m; i > 0; i--) {
-        if (i - arr[j] < 0) continue;
-        if (dp[i - arr[j]] === -Infinity) continue;
-        dp[i] = dp[i - arr[j]] + 1;
+for (let i = 1; i <= n; i++) {
+    for (let j = 0; j <= m; j++) {
+        // Case 1: i번째 원소를 선택하여 합이 j가 된 경우
+        if (j - arr[i] >= 0 && dp[i - 1][j - arr[i]]) { // i - 1번째까지 고려하여 고른 수의 합이 j - arr[i]
+            dp[i][j] = true;
+        }
+
+        // Case 2: i번째 원소를 선택하지 않고 합이 j가 된 경우
+        if (dp[i - 1][j]) {
+            dp[i][j] = true;
+        }
     }
 }
 
-console.log(dp[m] === -Infinity ? "No" : "Yes");
+// n개의 수까지 고려해봤을 때 정확히 합이 m이 되는지 판단
+console.log(dp[n][m] ? "Yes" : "No");
